7f95f6d26f69ef963245ef600e7725eb7ba22082
ahmedammar027 <ahmedh123ammar123@gmail.com>
2026-07-18 22:49:44 +0300

    Assignment

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Book
+    {
+
+        
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public override string ToString()
+        {
+            return $"Title: {Title} \n, Pages: {Pages}";
+        
+    }
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1
+       Book b1= new Book();
+            b1.Title = "Design pattern interview";
+            b1.Pages = 50;
+            Console.WriteLine(b1);
+            #endregion
+            #region Q2
+            Console.WriteLine(b1.ToString());
+            Console.WriteLine(b1.Equals(b1));
+            Console.WriteLine(b1.GetHashCode());
+            Console.WriteLine(b1.GetType());
+
+            #endregion
+            #region Q3
+            //  int pages = "464"; compile-time error
+            string pages = "464";
+            int x = Convert.ToInt32(pages);
+            Console.WriteLine(x);
+            #endregion
+            #region Q4
+           int y = 10;
+           int z = 0;
+            try {
+                Console.WriteLine(y / z);
+
+            }
+            catch {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            finally { Console.WriteLine("Done"); }
+            #endregion
+            #region Q5 
+            int pagees = 300;
+            double p= Convert.ToDouble(pagees);
+            Console.WriteLine(p);
+            #endregion
+            #region Q6
+            double price = 49.99;
+            int k = (int)price;
+            Console.WriteLine(k);
+            #endregion
+            #region Q7
+            string pagesText = "464";
+            int s=Convert.ToInt32(pagesText);
+            Console.WriteLine(s);
+            #endregion
+            #region Q8
+            string yearText = "2023";
+          int r= int.Parse(yearText);
+            Console.WriteLine(r.GetType());
+          string badText = "abc";
+            checked
+            {
+                int.TryParse(badText, out int se);
+            }
+            unchecked
+            {
+                Console.WriteLine("faild to convert");
+            }
+            #endregion
+            #region Q9 
+            int pagaes = 464;
+            string pp=pagaes.ToString();
+            Console.WriteLine(pp);
+            Console.WriteLine(pp.GetType());
+            #endregion
+            #region Q10
+
+            int copies = 100;
+            object obj=(object)copies;
+            Console.WriteLine(obj);
+            Console.WriteLine(obj.GetType());
+
+            int de=(int)obj;
+            Console.WriteLine(de);
+            Console.WriteLine(de.GetType());
+
+            #endregion
+            #region Q11
+            int? year = null;
+            if (year.HasValue) {
+                Console.WriteLine($" year is {year}");
+            }
+            else
+            {
+                year = 2023;
+                Console.WriteLine($" year is {year}");
+            }
+            #endregion
+            #region Q12
+            string? reviewer = null;
+            if (reviewer==null)
+            {
+                Console.WriteLine($" reviewer is null");
+            }
+            else
+            {
+                
+                Console.WriteLine($" reviewer is {reviewer}");
+            }
+
+            #endregion
+            #region Q13 
+             Book? book = null;
+            Console.WriteLine(book?.Title);
+
+
+            #endregion
+            #region Q14
+            Console.WriteLine(book?.Title ?? "Untitled");
+
+            book = new Book(); 
+            book.Title = null; 
+
+            book.Title ??= "Untitled";
+
+            Console.WriteLine(book.Title);
+            #endregion
+            #region Q15
+            string? name = "Ahmed";
+            string confirmedName =name!;
+            Console.WriteLine(name);
+            Console.WriteLine(confirmedName);
+            #endregion
+        }
+    }
+}
