f597faed445174a758a35ce90c02ccae668ac4c6
Ahmed Ammar <151404860+ahmedammar027@users.noreply.github.com>
2026-08-19 15:09:58 +0300

    Add files via upload
    G-ASP-NET-87-OOP04

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp1.ShipmentTyeps;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            /*overloading is method do same function with diffrent params
+            *overriding is same method do more function 
+             */
+            #endregion
+            #region Q2
+            /*static binding use method in ref type(parent) and use hiding method (new) and overloading (new) run in compile time and fast
+            *dynamic binding use method in obj type(child) and use override method  run in run time and slow
+            */
+            #endregion
+
+            StandardShipment s1 = new StandardShipment
+                ("001", "laptop", 15, 16, new DeliveryAddress("cairo", "maddi", 12));
+            ExpressShipment s2 = new ExpressShipment
+                (119, "002", "mobile", 15, 16, new DeliveryAddress("alex", "wardian", 13));
+            InternationalShipment s3 = new InternationalShipment
+                (15, "matrooh", "003", "tablet", 13, 55, new DeliveryAddress("cairo", "maddi", 12));
+
+            Shipment[] sh = new Shipment[] { s1, s2, s3 };
+
+        DeliveryCenter delivery = new DeliveryCenter(" Ahmed Mohamed",sh);
+
+
+            delivery.PrintAllShipments();
+        
+        
+        
+        
+        }
+    }
+    }
---
+++ ShipmentTyeps/ExpressShipment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.ShipmentTyeps
+{
+
+    public class ExpressShipment : Shipment
+    {
+        private decimal _extraFee;
+
+        public virtual decimal ExtraFee
+        {
+            get { return _extraFee; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "wrong");
+                }
+                _extraFee = value;
+            }
+        }
+
+        public override void PrintShipment()
+        {
+         
+            Console.WriteLine("========================================");
+            Console.WriteLine("           SHIPMENT DETAILS             ");
+            Console.WriteLine("========================================");
+            Console.WriteLine($"order type is : {this.GetType().Name}");
+            Console.WriteLine($"Tracking Code : {_TrackingCode}");
+            Console.WriteLine($"Description   : {_Description}");
+            Console.WriteLine($"Weight        : {_Weight} kg");
+            Console.WriteLine($"Delivery Fee  : {_DeliveryFee:C}");
+            Console.WriteLine($"extra fees  : {_extraFee:C}");
+            Console.WriteLine($"Destination   : {Destination.GetFullAddress()}");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost:C}");
+            Console.WriteLine("========================================");
+        
+        }
+
+        public ExpressShipment(decimal extraFee, string trackingCode, string description, int weight, int deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            ExtraFee = extraFee;
+        }
+
+        public override decimal EstimatedCost
+        {
+            get
+            {
+                return base.EstimatedCost + ExtraFee;
+            }
+        }
+        
+    }
+
+}
